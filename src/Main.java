7250fb3119f06527bbfbaeb4de68ed577f4e892b
ryanspencer26 <152317263+ryanspencer26@users.noreply.github.com>
2023-11-29 17:24:57 +0000

    I was on replit.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("FIRST MUKBANG - AsianGlue");
         System.out.println("I am Batman.");
+        System.out.println("I am on replit.");
 
     }
 
